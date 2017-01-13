c46dcfa7c7394c3107dd561282ab617be26854e4
etang4 <Erictangis@gmail.com>
2017-01-12 23:54:24 -0800

    Add good boilerplate

--- src/config.js
+++ src/config.js
@@ -15,21 +15,21 @@ module.exports = Object.assign({
   apiHost: process.env.APIHOST || 'localhost',
   apiPort: process.env.APIPORT,
   app: {
-    title: 'React Redux Example',
-    description: 'All the modern best practices in one example.',
+    title: 'Travelr',
+    description: 'The Traveler\'s Social Media',
     head: {
-      titleTemplate: 'React Redux Example: %s',
+      titleTemplate: 'Travelr %s',
       meta: [
-        {name: 'description', content: 'All the modern best practices in one example.'},
+        {name: 'description', content: 'The Traveler\'s Social Media'},
         {charset: 'utf-8'},
-        {property: 'og:site_name', content: 'React Redux Example'},
+        {property: 'og:site_name', content: 'Travelr'},
         {property: 'og:image', content: 'https://react-redux.herokuapp.com/logo.jpg'},
         {property: 'og:locale', content: 'en_US'},
-        {property: 'og:title', content: 'React Redux Example'},
-        {property: 'og:description', content: 'All the modern best practices in one example.'},
+        {property: 'og:title', content: 'Travelr'},
+        {property: 'og:description', content: 'The Traveler\'s Social Media'},
         {property: 'og:card', content: 'summary'},
-        {property: 'og:site', content: '@erikras'},
-        {property: 'og:creator', content: '@erikras'},
+        {property: 'og:site', content: '@etang4'},
+        {property: 'og:creator', content: '@etang4'},
         {property: 'og:image:width', content: '200'},
         {property: 'og:image:height', content: '200'}
       ]
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -5,10 +5,12 @@ import config from '../../config';
 import Helmet from 'react-helmet';
 
 export default class Home extends Component {
+
   render() {
     const styles = require('./Home.scss');
     // require the logo image both from client and server
     const logoImage = require('./logo.png');
+
     return (
       <div className={styles.home}>
         <Helmet title="Home"/>
